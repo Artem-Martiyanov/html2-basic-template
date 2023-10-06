a8cdd59bc6f5306f97f309c4a377d53ff9e3820c
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-10-06 20:49:24 +0300

    Adds header and footer

---
+++ source/js/burger.js
@@ -0,0 +1,13 @@
+import Constant from './constants.js'
+
+const burgerButton = document.querySelector('.burger')
+
+if (burgerButton && document.documentElement.clientWidth < Constant.VIEWPORT_TABLET) {
+  const menu = document.querySelector('.nav')
+  menu.classList.add('nav--hidden')
+
+  burgerButton.addEventListener('click', () => {
+    burgerButton.classList.toggle('burger--opened')
+    menu.classList.toggle('nav--hidden')
+  })
+}
