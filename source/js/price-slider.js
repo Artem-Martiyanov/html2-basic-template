c6d7e307b1e70f8c3fed8be54ec677915c0f106b
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-10-07 18:04:49 +0300

    Adds range slider

---
+++ source/js/price-slider.js
@@ -0,0 +1,23 @@
+const priceInputs = document.querySelectorAll('.filter-form__price-input')
+const priceSliderElement = document.querySelector('.range-slider')
+
+
+const priceSlider = noUiSlider.create(priceSliderElement, {
+  start: [0, 900],
+  connect: true,
+  range: JSON.parse(priceSliderElement.dataset.range),
+  format: {
+    to: (value) => value.toFixed(0),
+    from: (value) => value
+  }
+});
+
+priceSlider.on('update', (values, handle) => {
+  priceInputs[handle].value = values[handle]
+})
+
+priceInputs.forEach(input => {
+  input.addEventListener('change', () => {
+    priceSlider.set([priceInputs[0].value, priceInputs[1].value])
+  })
+})
--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,6 @@
+import './vendor/no-ui-slider/nouislider.min.js'
+
 import './burger.js'
 import './slider.js'
+import './price-slider.js'
+
